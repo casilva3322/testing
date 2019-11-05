19cc081b955ff37d3e5e8350e1094fe64424570a
Claudia Silva <casilva2@buffs.wtamu.edu>
2019-11-05 13:46:15 -0800

    anotherone

---
+++ Controllers/thisT.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using wellSomeTest.Models;
+using System.Web;
+using SaveFormData.Models;
+
+namespace wellSomeTest.Controllers
+{
+    public class thisTController : Controller
+    {
+                // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+ 
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+ 
+        // POST: Home/Create
+        [HttpPost]
+                public ActionResult Create(StudentModel smodel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    ViewData["Name"] = smodel.Name;
+                    ViewData["City"] = smodel.City;
+                    ViewData["Address"] = smodel.Address;
+ 
+                    return View("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+    }
+}
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,15 @@
+ using System;    
+    
+    
+    
+    
+    namespace SaveFormData.Models
+    {
+        public class StudentModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string City { get; set; }
+            public string Address { get; set; }
+        }
+    }
